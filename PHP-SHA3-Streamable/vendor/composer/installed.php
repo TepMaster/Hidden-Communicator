823aa583a0cf35a314a9730601b4b8d5a8d930dd
TepMaster <58990882+TepMaster@users.noreply.github.com>
2022-06-06 15:45:53 +0300

    SHA3-lib

---
+++ PHP-SHA3-Streamable/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b099bb81050c71eef7f42d46d287086a4949b861',
+        'name' => 'desktopd/php-sha3-streamable',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'desktopd/php-sha3-streamable' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b099bb81050c71eef7f42d46d287086a4949b861',
+            'dev_requirement' => false,
+        ),
+    ),
+);
